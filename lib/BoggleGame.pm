729d184673230d9bee199b574cc78a911e462a1b
Nathan G. Marley <nathan.marley@gmail.com>
2012-07-04 18:43:04 -0500

    Added input text box to keep track of found words. Words are stored in the session & 'found word' list is reset upon new game.

--- lib/BoggleGame.pm
+++ lib/BoggleGame.pm
@@ -1,14 +1,40 @@
 package BoggleGame;
 
 use Dancer;
+use Dancer::Plugin::Ajax;
 use Boggle;
 use Data::Dumper;
+use JSON;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
     my $game = Boggle->new();
+
+    if ( session('words') ) {
+        session words => '';
+    }
+
     template 'index', { faces => \@{$game->faces} };
+};
+
+
+# word list stored as arrayref in session
+ajax '/addword/:word' => sub {
+    my $words;
+
+    if ( session('words') ) {
+        $words = session('words');
+    }
+
+    my $new_word = param 'word'; 
+    unless ( $new_word ~~ @$words ) {
+        push @$words, $new_word;
+    }
+
+    session 'words' => $words;
+
+    return encode_json( $words );
 };
 
 true;
